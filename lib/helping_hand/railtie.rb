67ca91f127317e2f5170ad18785443fb9547b58b
Chris Oliver <excid3@gmail.com>
2015-05-09 22:37:17 -0400

    URI escape error and remove unused railtie

--- lib/helping_hand/railtie.rb
+++
@@ -1,9 +0,0 @@
-require 'rails/railtie'
-
-module WebConsole
-  class Railtie < ::Rails::Railtie
-    config.helping_hand = ActiveSupport::OrderedOptions.new
-    config.helping_hand.host = "https://helpinghand-rails.herokuapp.com/"
-  end
-end
-
