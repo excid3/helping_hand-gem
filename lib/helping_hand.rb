e3aa1ac70b6f1cfbf26dc383db2a5ebddd941adb
Chris Oliver <excid3@gmail.com>
2015-05-09 22:59:16 -0400

    Remove reference to railtie

--- lib/helping_hand.rb
+++ lib/helping_hand.rb
@@ -1,5 +1,4 @@
 require "helping_hand/version"
-require "helping_hand/railtie"
 
 # Override the error templates path from Rails to serve our modified versions
 ActionDispatch::DebugExceptions::RESCUES_TEMPLATE_PATH = File.expand_path('../templates', __FILE__)
