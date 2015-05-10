e79253b5792157b9b80ada75f964dae532f265df
Chris Oliver <excid3@gmail.com>
2015-05-09 23:42:24 -0400

    Add config and remove const change warning

--- lib/helping_hand.rb
+++ lib/helping_hand.rb
@@ -1,8 +1,25 @@
 require "helping_hand/version"
 
 # Override the error templates path from Rails to serve our modified versions
-ActionDispatch::DebugExceptions::RESCUES_TEMPLATE_PATH = File.expand_path('../templates', __FILE__)
+ActionDispatch::DebugExceptions.send(:remove_const, :RESCUES_TEMPLATE_PATH)
+ActionDispatch::DebugExceptions.const_set(:RESCUES_TEMPLATE_PATH, File.expand_path('../templates', __FILE__))
 
 module HelpingHand
-  HOST = "https://helpinghand-rails.herokuapp.com/"
+  class << self
+    attr_accessor :configuration
+  end
+
+  def self.config
+    self.configuration ||= Configuration.new
+    yield(configuration) if block_given?
+    self.configuration
+  end
+
+  class Configuration
+    attr_accessor :host
+
+    def initialize
+      @host = "https://helpinghand-rails.herokuapp.com/"
+    end
+  end
 end
